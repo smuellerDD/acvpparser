00ed132b26997dc96f0a1f3ce66fbd1a145bca19
Stephan Mueller <smueller@chronox.de>
2021-05-17 08:19:37 +0200

    bump version to 2.1.2
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- parser/parser.h
+++ parser/parser.h
@@ -46,7 +46,7 @@ extern "C"
 			* functional enhancements only, consumer
 			* can be left unchanged if enhancements are
 			* not considered. */
-#define PATCHLEVEL 1   /* API / ABI compatible, no functional
+#define PATCHLEVEL 2   /* API / ABI compatible, no functional
 			* changes, no enhancements, bug fixes
 			* only. */
 
