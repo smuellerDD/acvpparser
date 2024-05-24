83d342d6e9b0cb53368f96f20aa2060b9e3492d2
Vikash Bansal <bvikas@vmware.com>
2024-05-24 11:40:06 +0530

    libkcapi: added support for symmetric AES cipher encryption,  decryption and Monte Carlo Test
    sym_cipher - perform AES cipher selection
    sym_encrypt - perform AES encryption
    sym_decrypt - perform AES decryption
    
    Co-authored-by: Srish Srinivasan <srish.srinivasan@broadcom.com>

--- backends/backend_libkcapi.c
+++ backends/backend_libkcapi.c
@@ -240,7 +240,7 @@ static int rsa_sigver(struct rsa_sigver_data *data, flags_t parsed_flags)
 
         CKINT_LOG(alloc_buf(dgst_len, &mac), "rsa: mac buffer could not be allocated\n");
         ret = kcapi_md_digest(handle1, data->msg.buf, data->msg.len, mac.buf, mac.len);
-        if (0 > ret)
+        if (ret < 0)
         {
                 logger(LOGGER_ERR, "rsa: message digest generation failed\n");
                 goto out;
@@ -401,7 +401,7 @@ static int rsa_siggen(struct rsa_siggen_data *data,
         CKINT_LOG(alloc_buf(dgst_len, &mac), "rsa: mac buffer cannot be allocated\n");
         ret = kcapi_md_digest(handle1, data->msg.buf, data->msg.len, mac.buf, mac.len);
 
-        if (0 > ret)
+        if (ret < 0)
         {
                 logger(LOGGER_ERR, "rsa: message digest generation failed\n");
                 goto out;
@@ -529,4 +529,305 @@ ACVP_DEFINE_CONSTRUCTOR(kcapi_rsa_backend)
 static void kcapi_rsa_backend(void)
 {
         register_rsa_impl(&kcapi_rsa);
+}
+
+static int sym_cipher(uint64_t acvp_cipher, char* cipher)
+{
+        switch (acvp_cipher)
+        {
+                case ACVP_ECB:
+                        strcpy(cipher, "ecb(aes)");
+                        break;
+                case ACVP_CBC:
+                        strcpy(cipher, "cbc(aes)");
+                        break;
+                case ACVP_CBC_CS3:
+                        strcpy(cipher, "cts(cbc(aes))");
+                        break;
+                case ACVP_CFB128:
+                        strcpy(cipher, "cfb(aes)");
+                        break;
+                case ACVP_CTR:
+                        strcpy(cipher, "ctr(aes)");
+                        break;
+                case ACVP_XTS:
+                        strcpy(cipher, "xts(aes)");
+                        break;
+                case ACVP_TDESECB:
+                        strcpy(cipher, "ecb(des3_ede)");
+                        break;
+                case ACVP_TDESCBC:
+                        strcpy(cipher, "cbc(des3_ede)");
+                        break;
+                case ACVP_TDESCTR:
+                        strcpy(cipher, "ctr(des3_ede)");
+                        break;
+                default:
+                        return -EINVAL;
+        }
+
+        return 0;
+}
+
+static int sym_encrypt(struct sym_data *data, flags_t parsed_flags)
+{
+        int ret=0;
+        struct kcapi_handle *handle = NULL;
+        struct buffer pt;
+        char cipher[CIPHERMAXNAME];
+
+        if(!data)
+        {
+                logger(LOGGER_ERR,"sym: sym_data is empty, returning -EINVAL...\n");
+                return -EINVAL;
+        }
+        (void)parsed_flags;
+
+        if(sym_cipher(data->cipher,cipher))
+        {
+                logger(LOGGER_ERR, "sym: invalid cipher\n");
+                return -EINVAL;
+        }
+
+        pt.len = data->data.len;
+        pt.buf = data->data.buf;
+
+        if(kcapi_cipher_init(&handle, cipher, 0)) {
+                logger(LOGGER_ERR, "sym: cipher init failed\n");
+                return -EINVAL;
+        }
+
+        data->data.len = 0;
+        data->data.buf = NULL;
+        CKINT_LOG(alloc_buf(pt.len, &data->data), "sym: buffer for plain text could not be allocated\n");
+
+        if (!data->key.len ||
+                !data->key.buf ||
+                (ret = kcapi_cipher_setkey(handle, data->key.buf, data->key.len)))
+        {
+                logger(LOGGER_ERR, "sym: setting key failed with error = %d\n", ret);
+                goto out;
+        }
+
+        ret = kcapi_cipher_encrypt(handle,
+                        pt.buf, pt.len,
+                        data->iv.buf,
+                        data->data.buf, data->data.len,
+                        KCAPI_ACCESS_SENDMSG);
+
+        /* error only when ret < 0, success in all other cases */
+        if(ret > 0)
+                ret=0;
+
+out:
+        if(pt.buf)
+                free_buf(&pt);
+        kcapi_cipher_destroy(handle);
+        return ret;
+}
+
+static int sym_decrypt(struct sym_data *data, flags_t parsed_flags)
+{
+        int ret = 0;
+        struct kcapi_handle *handle = NULL;
+        struct buffer ct;
+        char cipher[CIPHERMAXNAME];
+
+        if(!data)
+        {
+                logger(LOGGER_ERR, "sym: sym_data is empty, returning -EINVAL...\n");
+                return -EINVAL;
+        }
+        (void)parsed_flags;
+
+        if(sym_cipher(data->cipher, cipher))
+        {
+                logger(LOGGER_ERR, "sym: invalid cipher\n");
+                return -EINVAL;
+        }
+
+        ct.len = data->data.len;
+        ct.buf = data->data.buf;
+
+        data->data.len = 0;
+        data->data.buf = NULL;
+        CKINT_LOG(alloc_buf(ct.len, &data->data), "sym: buffer for cipher text could not be allocated\n");
+
+        if(kcapi_cipher_init(&handle, cipher, 0))
+        {
+                logger(LOGGER_ERR, "sym: cipher init Failed\n");
+                return -EINVAL;
+        }
+
+        if (!data->key.len ||
+                !data->key.buf ||
+                (ret = kcapi_cipher_setkey(handle, data->key.buf, data->key.len)))
+        {
+                logger(LOGGER_ERR, "sym: setting key failed with error = %d\n", ret);
+                goto out;
+        }
+
+        ret = kcapi_cipher_decrypt(handle,
+                        ct.buf, ct.len,
+                        data->iv.buf,
+                        data->data.buf, data->data.len,
+                        KCAPI_ACCESS_SENDMSG);
+
+        /* error only when ret < 0, success in all other cases */
+        if(ret > 0)
+                ret = 0;
+
+out:
+        if(ct.buf)
+                free_buf(&ct);
+        kcapi_cipher_destroy(handle);
+        return ret;
+}
+
+struct buffer old;
+char orig_iv[500];
+int orig_iv_len;
+
+static int sym_mct_init(struct sym_data *data, flags_t parsed_flags)
+{
+        int ret=0;
+        struct kcapi_handle *handle = NULL;
+        char cipher[CIPHERMAXNAME];
+
+        if(!data)
+        {
+                logger(LOGGER_ERR, "sym: sym_data is empty, returning -EINVAL...\n");
+                return -EINVAL;
+        }
+        (void)parsed_flags;
+
+        if(old.buf)
+                free_buf(&old);
+
+        old.buf = NULL;
+        old.len = 0;
+
+        if(sym_cipher(data->cipher, cipher))
+        {
+                logger(LOGGER_ERR, "sym: invalid cipher\n");
+                return -EINVAL;
+        }
+
+        if(kcapi_cipher_init(&handle, cipher, 0))
+        {
+                logger(LOGGER_ERR, "sym: cipher init Failed\n");
+                return -EINVAL;
+        }
+
+        if (!data->key.len || !data->key.buf ||
+                (ret = kcapi_cipher_setkey(handle, data->key.buf, data->key.len)))
+        {
+                logger(LOGGER_ERR, "sym: setting key failed with error = %d\n", ret);
+                kcapi_cipher_destroy(handle);
+                goto out;
+        }
+
+        if(data->iv.buf)
+                memcpy(orig_iv, data->iv.buf, data->iv.len);
+
+        data->priv = (void*)handle;
+
+out:
+        return ret;
+}
+
+static int sym_mct_update(struct sym_data *data, flags_t parsed_flags)
+{
+        int ret=0;
+        struct kcapi_handle *handle = NULL;
+        struct buffer in;
+        size_t origlen = data->data.len;
+
+        if(!data)
+        {
+                logger(LOGGER_ERR, "sym: sym_data is empty, returning -EINVAL...\n");
+                return -EINVAL;
+        }
+        (void)parsed_flags;
+
+        handle = (struct kcapi_handle *)(data->priv);
+        in.len = data->data.len;
+        in.buf = data->data.buf;
+
+        if(!(old.buf) && (data->iv.len != 0))
+        {
+                CKINT_LOG(alloc_buf(data->data.len, &old), "sym: old buffer could not be allocated\n");
+                if(data->iv.buf)
+                        memcpy(old.buf, data->iv.buf, old.len);
+        }
+
+        data->data.len = 0;
+        data->data.buf = NULL;
+        CKINT_LOG(alloc_buf(in.len, &data->data), "sym: data buffer could not be allocated\n");
+
+        if (parsed_flags & FLAG_OP_ENC)
+        {
+                ret = kcapi_cipher_encrypt(handle,
+                                in.buf, in.len,
+                                old.buf,
+                                data->data.buf, data->data.len,
+                                KCAPI_ACCESS_SENDMSG);
+                if(old.buf && data->data.buf)
+                        memcpy(old.buf, data->data.buf, old.len);
+        }
+        else
+        {
+                ret = kcapi_cipher_decrypt(handle,
+                                in.buf, in.len,
+                                old.buf,
+                                data->data.buf, data->data.len,
+                                KCAPI_ACCESS_SENDMSG);
+                if(old.buf && in.buf)
+                        memcpy(old.buf, in.buf, in.len);
+        }
+
+        if (data->data.len != origlen)
+                data->data.len = origlen;
+
+out:
+        if(in.buf)
+                free_buf(&in);
+        return ret;
+}
+
+static int sym_mct_fini(struct sym_data *data, flags_t parsed_flags)
+{
+        struct kcapi_handle *handle = NULL;
+
+        if(!data)
+        {
+                logger(LOGGER_ERR, "sym: sym_data is empty, returning -EINVAL...\n");
+                return -EINVAL;
+        }
+        (void)parsed_flags;
+
+        handle = (struct kcapi_handle *)(data->priv);
+
+        if(handle)
+                kcapi_cipher_destroy(handle);
+
+        if(old.buf)
+                free_buf(&old);
+
+        return 0;
+}
+
+static struct sym_backend kcapi_sym =
+{
+        sym_encrypt,
+        sym_decrypt,
+        sym_mct_init,
+        sym_mct_update,
+        sym_mct_fini
+};
+
+ACVP_DEFINE_CONSTRUCTOR(kcapi_sym_backend)
+static void kcapi_sym_backend(void)
+{
+        register_sym_impl(&kcapi_sym);
 }
